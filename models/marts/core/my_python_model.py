1742a80414ae43a85ccf8354e9bad2802e137ff4
melikjava <melik.akyildiz@hotmail.com>
2022-10-27 16:42:06 +0200

    meta has been added to the table

--- models/marts/core/my_python_model.py
+++ models/marts/core/my_python_model.py
@@ -4,6 +4,6 @@ def model(dbt, session):
 
     my_sql_model_df = dbt.ref("dim_customers")
 
-    final_df = my_sql_model_df  # stuff you can't write in SQL!
+   # final_df =   # stuff you can't write in SQL!
 
-    return final_df
+    return my_sql_model_df
