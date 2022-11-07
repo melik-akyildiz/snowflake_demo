de075c15f5c9dcd5e0b2695bb97c5c8c84d122d1
melikjava <melik.akyildiz@hotmail.com>
2022-11-07 14:00:58 +0100

    Python

--- models/marts/core/my_python_model.py
+++ models/marts/core/my_python_model.py
@@ -1,8 +1,26 @@
+import snowflake.snowpark.types as T
+import snowflake.snowpark.functions as F
+import numpy
+def register_udf_add_random():
+    add_random = F.udf(
+        # use 'lambda' syntax, for simple functional behavior
+        lambda x: x + numpy.random.normal(),
+        return_type=T.FloatType(),
+        input_types=[T.FloatType()]
+    )
+    return add_random
+
 def model(dbt, session):
-    target_name = dbt.config.get("target_name")
-    specific_var = dbt.config.get("specific_var")
-    specific_env_var = dbt.config.get("specific_env_var")
+    dbt.config(
+        materialized = "table",
+        packages = ["numpy"]
+    )
 
-    orders_df = dbt.ref("fct_orders")
+temps_df = dbt.ref("fct_orders")
 
-    return orders_df
+add_random = register_udf_add_random()
+
+# warm things up, who knows by how much
+df = temps_df.withColumn("order_key", add_random("degree"))
+
+return df
