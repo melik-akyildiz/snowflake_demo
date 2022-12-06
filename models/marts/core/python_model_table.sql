d6a13501814a1d400f824982ee159c27758a0ac2
melikjava <melik.akyildiz@hotmail.com>
2022-12-06 15:10:39 +0100

    python change

--- models/marts/core/python_model_table.sql
+++ models/marts/core/python_model_table.sql
@@ -1,6 +1,46 @@
+{{
+    config(
+        materialized = 'table',
+        transient=false
+    )
+}}
+
+with PYTHON_MODEL_TABLE as (
+
+    select * from {{ ref('stg_tpch_customers') }}
+
+),
+nation as (
+
+    select * from {{ ref('stg_tpch_nations') }}
+),
+region as (
+
+    select * from {{ ref('stg_tpch_regions') }}
+
+),
+final as (
+    select
+        PYTHON_MODEL_TABLE.customer_key,
+        PYTHON_MODEL_TABLE.name,
+        PYTHON_MODEL_TABLE.address,
+        {# nation.nation_key as nation_key, #}
+        nation.name as nation,
+        {# region.region_key as region_key, #}
+        region.name as region,
+        PYTHON_MODEL_TABLE.phone_number,
+        PYTHON_MODEL_TABLE.account_balance,
+        PYTHON_MODEL_TABLE.market_segment
+    from
+        PYTHON_MODEL_TABLE
+        inner join nation
+            on PYTHON_MODEL_TABLE.nation_key = nation.nation_key
+        inner join region
+            on nation.region_key = region.region_key
+)
 select
-     *
- from
-     PYTHON_MODEL_TABLE
- order by
-     customer_key
+    *
+from
+    final
+order by
+    customer_key
