5bdfe67d90afa87fc002fd48e4a8c352991db064
melikjava <melik.akyildiz@hotmail.com>
2022-12-06 15:36:17 +0100

    python change

--- models/python/python_model_view.py
+++ models/python/python_model_view.py
@@ -1,7 +1,6 @@
 def model(dbt, session):
-    df = dbt.ref('python_model_table')
+    target_name = dbt.config.get("PYTHON_MODEL_TABLE")
     target_df =dbt.this()
 
-    target_df['customer_key'] = df['customer_key']
 
     return target_df
