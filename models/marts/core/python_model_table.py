8463c4fb8b58256bc4c641bad61f805a756be38b
melikjava <melik.akyildiz@hotmail.com>
2022-12-06 13:57:27 +0100

    Python model added same name as snowflake

--- models/marts/core/python_model_table.py
+++ models/marts/core/python_model_table.py
@@ -1,9 +1,3 @@
 def model(dbt, session):
-    dbt.config(
-        materialized='table',
-        transient=false
-    )
-
-    temps_df = dbt.ref('dim_customers')
-
-    return temps_df
+    df = dbt.ref('dim_customers')
+    return df
