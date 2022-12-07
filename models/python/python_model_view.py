e24ac1d71e41e88e180b217337dc01c62fef513d
melikjava <melik.akyildiz@hotmail.com>
2022-12-07 13:51:18 +0100

    python change

--- models/python/python_model_view.py
+++ models/python/python_model_view.py
@@ -1,18 +1,18 @@
 def model(dbt,session):
 
-        dbt.config(
-            materialized='table'
-        )
+           dbt.config(
+               materialized='table'
+           )
 
-        # these are DAG-aware, and return dataframes
-        dim_all_learners = dbt.ref("orders")
-        source_users = dbt.source("tpch", "customer")
+           # these are DAG-aware, and return dataframes
+           dim_all_learners = dbt.ref("orders")
+           source_users = dbt.source("tpch", "customer")
 
-        sample_command = dim_all_learners.limit(1000)
-                           # .filter(col("o_orderkey"=="1"))
+           sample_command = dim_all_learners.limit(1000)
+                              # .filter(col("o_orderkey"=="1"))
 
 
-        # your final 'select' statement
-        df = sample_command.select("*")
+           # your final 'select' statement
+           df = sample_command.select("*")
 
 return df['o_orderkey']
