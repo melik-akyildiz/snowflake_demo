4bc961a06d6a80d6f3f4971709221166213bb2c7
melikjava <melik.akyildiz@hotmail.com>
2022-12-07 14:02:49 +0100

    python change

--- models/python/python_model_view.py
+++ models/python/python_model_view.py
@@ -8,12 +8,12 @@ def model(dbt, session):
            dim_all_learners = dbt.ref("orders")
            source_users = dbt.source("tpch", "customer")
 
-           sample_command = dim_all_learners.limit(1)
+           #sample_command = dim_all_learners.limit(1)
                               # .filter(col("o_orderkey"=="1"))
 
 
            # your final 'select' statement
-           df = sample_command.select("*")
+           df = dim_all_learners.select("*")
 
            target_df['o_orderkey'] = dim_all_learners['o_orderkey']
 
