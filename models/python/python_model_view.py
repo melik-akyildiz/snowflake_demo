b5b355bc50d978357bf3b381a1e2c794b5eec02e
melikjava <melik.akyildiz@hotmail.com>
2022-12-07 14:04:29 +0100

    python change

--- models/python/python_model_view.py
+++ models/python/python_model_view.py
@@ -6,14 +6,14 @@ def model(dbt, session):
 
            # these are DAG-aware, and return dataframes
            dim_all_learners = dbt.ref("orders")
-           source_users = dbt.source("tpch", "customer")
+           #source_users = dbt.source("tpch", "customer")
 
            #sample_command = dim_all_learners.limit(1)
                               # .filter(col("o_orderkey"=="1"))
 
 
            # your final 'select' statement
-           #df = dim_all_learners.select("*")
+           #df = sample_command.select("*")
 
            target_df['o_orderkey'] = dim_all_learners['o_orderkey']
 
