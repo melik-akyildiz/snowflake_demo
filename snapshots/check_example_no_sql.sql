f6875affd2f93d1db21714469b35b062cfa20da6
melikjava <melik.akyildiz@hotmail.com>
2022-12-14 16:23:38 +0100

    snapshot

--- snapshots/check_example_no_sql.sql
+++ snapshots/check_example_no_sql.sql
@@ -1,5 +1,13 @@
 {% snapshot orders_snapshot_check %}
 
+    {{
+        config(
+          target_schema='PUBLIC',
+          strategy=['check','timestamp'],
+          unique_key='O_ORDERKEY',
+          check_cols=['O_ORDERSTATUS',all],
+        )
+    }}
 
 select * from {{ source('tpch', 'orders') }}
 
