59d989b1c31988e8e55cd115a8307c7a7154de9e
melikjava <melik.akyildiz@hotmail.com>
2022-12-14 14:13:59 +0100

    snapshot

---
+++ snapshots/check_example.sql
@@ -0,0 +1,14 @@
+{% snapshot orders_snapshot_check %}
+
+    {{
+        config(
+          target_schema='snapshots',
+          strategy='check',
+          unique_key='id',
+          check_cols=['status', 'is_cancelled'],
+        )
+    }}
+
+select * from {{ source('jaffle_shop', 'orders') }}
+
+    {% endsnapshot %}
