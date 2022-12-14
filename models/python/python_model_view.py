9e48fd027763471d02a588bd4dee69e8016e058d
melikjava <melik.akyildiz@hotmail.com>
2022-12-14 16:32:22 +0100

    snapshot

--- models/python/python_model_view.py
+++ models/python/python_model_view.py
@@ -2,7 +2,7 @@ def model(dbt, session):
 
                # these are DAG-aware, and return dataframes
                dim_all_learners = dbt.ref("python_model_table")
-               source_users = dbt.source("tpch", "customer")
+               source_users = dbt.source('tpch', 'orders')
 
                sample_command = dim_all_learners.limit(1)
                                   # .filter(col("o_orderkey"=="1"))
